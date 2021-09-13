2af1c533281196ede7d71f30bfb509a94df6992e
Pushkarlonkar <pushkar.lonkar21@gmail.com>
2021-09-13 10:58:25 +0530

    Updates

---
+++ precache-manifest.99bc8778b5dcc83309b76c9db38906e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d141b99584d112646d31a7d29f1019",
+    "url": "/Dropblocks/index.html"
+  },
+  {
+    "revision": "a3bfdf07c4dfd9a575b0",
+    "url": "/Dropblocks/static/css/main.5a8b0e4d.chunk.css"
+  },
+  {
+    "revision": "d60a7bc8e7516e7c6315",
+    "url": "/Dropblocks/static/js/2.7d95fd97.chunk.js"
+  },
+  {
+    "revision": "a3bfdf07c4dfd9a575b0",
+    "url": "/Dropblocks/static/js/main.628e7492.chunk.js"
+  },
+  {
+    "revision": "ba2101bc0818c68a3c29",
+    "url": "/Dropblocks/static/js/runtime-main.bd880fc3.js"
+  },
+  {
+    "revision": "6e8a0c90b822429b500624d695f10482",
+    "url": "/Dropblocks/static/media/cloud.6e8a0c90.png"
+  }
+]);
